2dd8dec2f92aaa5f2362de8605b049053dc6a506
kaveesha <kaveeshadilshani1@gmail.com>
2018-03-03 19:15:50 +0530

    sample node table created

---
+++ Database/create/node1.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `node1`;
+CREATE TABLE IF NOT EXISTS `node1` (
+  `Time` datetime DEFAULT NULL,
+  `Status` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
