fa29e29342593971e1e6c9644dcad0b1dc9568d2
DhanushkiMapitigama <dhanumapitigama@gmail.com>
2018-10-26 21:03:47 +0530

    Delete tbl_user_account.sql

--- Database/create/tbl_user_account.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tbl_user_account` (
-  `ID` char(32) COLLATE utf8_unicode_ci NOT NULL,
-  `USER_NAME` char(128) COLLATE utf8_unicode_ci NOT NULL,
-  
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `USER_NAME` (`USER_NAME`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
